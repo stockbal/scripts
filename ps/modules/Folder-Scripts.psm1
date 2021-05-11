b422ca8e70e61a70c333bfd48c293d2d950d54a4
stockbal <ludwig.stockbauer-muhr@msg.group>
2021-05-11 17:04:01 +0200

    refactor: replace alias of cmdlets + add short documentation

--- ps/modules/Folder-Scripts.psm1
+++ ps/modules/Folder-Scripts.psm1
@@ -1,35 +1,43 @@
+<#
+.SYNOPSIS
+    Determination of folder sizes in a given folder
+.DESCRIPTION
+    Determination of the sizes of the direct sub folders
+    of a given folder
+#>
 Function Get-FolderSizes() {
     [CmdletBinding()]
     param(
+        # Path to folder
         [Parameter(Mandatory)]
-        [string]$FolderName
+        [string]$FolderPath
     )
     $Files = @()
-    $FileCount = (gci -Path $FolderName).Count
+    $FileCount = (Get-ChildItem -Path $FolderPath).Count
     $AllFilesLength = 0
     if ($FileCount -eq 0) {
-        "Folder $FolderName does not contain any files/folder"
+        Write-Output "Folder $FolderPath does not contain any files/folder"
         return
     }
     # Start size determination
     $i = 0;
-    gci -force $FolderName -ErrorAction SilentlyContinue | ? { $_ -is [io.directoryinfo] } | % { 
+    Get-ChildItem -force $FolderPath -ErrorAction SilentlyContinue | Where-Object { $_ -is [io.directoryinfo] } | ForEach-Object { 
         $i++
         $percentComplete = $i / $FileCount * 100
         $percentText = '{0:N1}' -f $percentComplete
-	    Write-Progress -Activity "Determining Folder/File Sizes" -Status "$percentText% Complete" -PercentComplete $percentComplete;
-	    $len = 0
-	    gci -recurse -force $_.fullname -ErrorAction SilentlyContinue | % { $len += $_.length }
+        Write-Progress -Activity "Determining Folder/File Sizes" -Status "$percentText% Complete" -PercentComplete $percentComplete;
+        $len = 0
+        Get-ChildItem -recurse -force $_.fullname -ErrorAction SilentlyContinue | ForEach-Object { $len += $_.length }
         $AllFilesLength += $len
-	    $Row = "" | Select Name, Size, SizeInt
-	    $Row.Name = $_.Name
-	    #$Row.Size = 
+        $Row = "" | Select-Object Name, Size, SizeInt
+        $Row.Name = $_.Name
+        #$Row.Size = 
         $Row.SizeInt = $len / 1Mb
         $Row.Size = '{0:N2} MB' -f $Row.SizeInt
-	    $Files += $Row
+        $Files += $Row
     }
 
-    $Files | Sort-Object -Property SizeInt | FT Name, Size
-    write "-------------------------------------------------"
-    write "Size of all files/folder:", ('{0:N2} MB' -f ($AllFilesLength / 1Mb))
+    $Files | Sort-Object -Property SizeInt | Format-Table Name, Size
+    Write-Output "-------------------------------------------------"
+    Write-Output "Size of all files/folder:", ('{0:N2} MB' -f ($AllFilesLength / 1Mb))
 }
