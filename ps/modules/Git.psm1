b3bce404e397e971e085707ba9d95b9b58360bfe
stockbal <ludwig.stockbauer-muhr@msg.group>
2021-06-07 17:48:57 +0200

    feat: add new git cmdlets

--- ps/modules/Git.psm1
+++ ps/modules/Git.psm1
@@ -1,5 +1,6 @@
-######################################
-# Checks if the directory is a git directory
+<# 
+    Checks if the directory is a git directory
+#>
 Function isGit() {
     param(
         [Parameter()]
@@ -13,117 +14,68 @@ Function isGit() {
     return $false
 }
 
-#######################################
-# Checks if the given branch exists in the given remote
+<#
+    Checks if the given branch exists in the given remote
+#>
 Function existsRemoteBranch() {
     param(
         [Parameter()]
-        [string]$gitPath, 
+        [string]$GitPath, 
         [Parameter()]
-        [string]$remote, 
+        [string]$Remote, 
         [Parameter()]
-        [string]$branch
-    )
-    Set-Location gitPath
-    $branchExists = ($(git ls-remote --heads $remote $branch ) -ne "")
-    Set-Location ..
-    return $branchExists
-}
-#######################################
-# Retrieves the full path of an i18n folder inside the 
-# provided path
-Function getI18nFolder([string]$path) {
-    (Get-ChildItem $path -Recurse) | Foreach-Object {
-        if ($_.Name -eq "i18n") {
-            return $_.FullName
-        }
-    }
-}
-
-Function copyI18nFolder() {
-    param(
-        [Parameter()]
-        [string]$Path, 
-        [Parameter()]
-        [string]$TargetFolder, 
-        [Parameter()]
-        [bool]$CreateTarget
-    )
-    $repoName = (Split-Path $Path -Leaf)
-    $i18n = getI18nFolder -path $Path
-    
-    # Should the target folder be created ?
-    if ($CreateTarget) { 
-        new-item -ItemType Directory $TargetFolder\$repoName -Force
-    }
-    # Copy Folder to new path
-    if ($i18n) {
-        Copy-Item -Path $i18n -Recurse -Destination $TargetFolder\$repoName -ErrorAction SilentlyContinue
-    }
-}
-
-#######################################
-# Exports i18n folders from a ui5 application
-# to another location
-Function Export-UI5Translations() {
-    [CmdletBinding()]
-    param(
-        [Parameter(Mandatory)]
-        [string]$Path,
-        [Parameter(Mandatory)]
-        [string]$Target,
-        [Parameter()]
-        [bool]$CreateTarget = $true
-    )
-
-    if (Test-Path $Path\package.json) {
-        # this is obviously an app directory
-        copyI18nFolder -Path (Resolve-Path $Path).Path -TargetFolder $Target -CreateTarget $CreateTarget
-    }
-    else {
-        (Get-ChildItem -Path $Path) | Foreach-Object {
-            copyI18nFolder -Path $_.FullName -TargetFolder $Target -CreateTarget $CreateTarget
-        }
-    }
-}
-
-Function Get-ExistsGitRemoteBranch() {
-    [CmdletBinding()]
-    param(
-        [Parameter(Mandatory)]
-        [string]$Path,
-        [Parameter(Mandatory)]
-        [string]$Remote,
-        [Parameter(Mandatory)]
         [string]$Branch
     )
+    $currentLocation = Get-Location
+    Set-Location $GitPath
+    $branchExists = $null -ne (git ls-remote --heads $Remote $Branch 2>0)
+    Set-Location $currentLocation
+    return $branchExists
+}
 
-    if (!$Remote) {
-        $Remote = "origin"
-    }
+<#
+.SYNOPSIS
+    Tests if the given given git repository has a certain
+    remote branch
+#>
+Function Test-GitRemoteBranchExists() {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)]
+        [string]$Path,
+        [Parameter(Mandatory = $false)]
+        [string]$Remote = "origin",
+        [Parameter(Mandatory = $false)]
+        [string]$Branch = "main"
+    )
+
+    $Path = Resolve-Path $Path
 
     if (isGit($Path)) {
-        if (existsRemoteBranch($Path, $Remote, $Branch)) {
-            "Remote Branch '$Branch' exists"
-            return
-        }
+        return existsRemoteBranch -GitPath $Path -Remote $Remote -Branch $Branch
     }
     else {
-        # perform concurrent determination of the remote branch
-        #$resultDict = [System.Collections.Concurrent.ConcurrentDictionary[string,object]]::new();
-        #Get-ChildItem -Path . | ForEach-Object -parallel {
-        #    Set-Location $_
-        #    "Determining branch info for $_"
-        #    $branch_exists = 
-        #    $repo_info = "" | Select release_2011
-        #    If ($branch_exists) { $repo_info.release_2011 = "X" }
-        #    $dict = $using:resultDict
-        #    [void]$dict.TryAdd($_.Name, $repo_info)
-        #    Set-Location ..
-        #}
-        #$resultDict
+        Write-Error "There is not git repository at path $Path"
     }
-    
+    return $false    
+}
+
+<#
+.SYNOPSIS
+    Resets the local changes in a repository and retrieves the current
+    information from the current remote branch
+#>
+Function Reset-GitLocal() {
+    [CmdletBinding()]
+    param (
+        # Path to a valid Git repository
+        [Parameter(Mandatory = $true)]
+        [string] $Path,
+        # Optional name of branch to which the repository should be resetted to
+        [Parameter()]
+        [string] $Branch
+    )
+
 }
 
 Export-ModuleMember *-*
